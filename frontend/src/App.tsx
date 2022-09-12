62e8c7ae894b6440eb6824da05e36b56d82eafeb
Jonathan pereira <jonathanp.carvalhorj@gmail.com>
2022-09-11 22:39:10 -0300

    Finalizado integracao com backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (<h1>Olá mundo!</h1>)
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+
+
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,26 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+import axios from 'axios'
+import { BASE_URL } from './../../utils/request';
+import { toast } from 'react-toastify';
+
+type Props = {
+  saleId:number;
+}
+
+function handleClick(saleId : number) {
+  axios.get(`${BASE_URL}/sales/${saleId}/notification`).then(response => {
+    toast.info("SMS enviado com sucesso");
+  });
+}
+
+function NotificationButton({saleId}: Props) {
+
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="Notificar" />
+    </div>
+  )
+}
+
+export default NotificationButton
